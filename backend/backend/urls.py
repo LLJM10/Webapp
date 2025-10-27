738982e8b41fe350b1a7b92bbaffd5d14d568e93
lukasn525 <lukasniessen05@gmail.com>
2025-10-27 14:59:48 +0100

    fixed

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import auto_admin  
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
