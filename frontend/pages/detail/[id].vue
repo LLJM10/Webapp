769752a8ea82528d26c82d306d598a94fc4c480c
lukasn525 <lukasniessen05@gmail.com>
2025-12-02 11:05:58 +0100

    Kontakt aufnehmen Button

--- frontend/pages/detail/[id].vue
+++ frontend/pages/detail/[id].vue
@@ -137,10 +137,7 @@
               </svg>
               Pitch merken
             </button>
-            <button class="btn ghost" style="width: 100%" @click="dummyApi('/api/contact?pitch=' + pitch.id)">
-              <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16" style="margin-right: 8px">
-                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
-              </svg>
+            <button @click="openMail" class="btn ghost" style="width: 100%">
               Kontakt aufnehmen
             </button>
           </div>
@@ -198,6 +195,11 @@ import PaymentButton from '~/components/PaymentButton.vue';
 const route = useRoute();
 const pitch = ref(null);
 const paymentAmount = ref('10.00');
+
+
+function openMail() {
+  window.location.href = 'mailto:startup@test.de'
+}
 
 // Helper function to get the correct image URL
 function getImageUrl(imgPath, fallbackText = 'Pitch') {
